b19192da5883f3f2ea695885faa188df0f7786fa
shivansh <makhijashivansh14@gmail.com>
2023-03-20 17:29:15 +0530

    project - 1

---
+++ python projevt code.py
@@ -0,0 +1,161 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:\Users\HP\OneDrive\Desktop\cse\code 3.py =============
+                        ************************************
+                        -->> Employee Management System <<--
+                        ************************************
+1. Add Employee
+2. Display Employee Record
+3. Update Employee Record
+4. Promote Employee Record
+5. Remove Employee Record
+6. Search Employee Record
+7. Exit
+
+                   -->> Choice Options: [1/2/3/4/5/6/7] <<--
+Enter your Choice: 1
+                                         Add Employee Record
+Enter Employee Id: 25
+Enter Employee Name: Shivansh Banyal
+Enter Employee Email ID: shivanshbania@gmail.com
+Valid Email
+Enter Employee Phone No.: 9816034567
+Valid Phone Number
+Enter Employee Address: SunderNagar Himachal Pradesh
+Enter Employee Post: Software Developer 
+Enter Employee Salary: 3000000
+Successfully Added Employee Record
+Press Any Key To Continue..f
+                        ************************************
+                        -->> Employee Management System <<--
+                        ************************************
+1. Add Employee
+2. Display Employee Record
+3. Update Employee Record
+4. Promote Employee Record
+5. Remove Employee Record
+6. Search Employee Record
+7. Exit
+
+                   -->> Choice Options: [1/2/3/4/5/6/7] <<--
+Enter your Choice: 2
+                           -->> Display Employee Record <<--
+Employee Id:  1
+Employee Name:  shivansh
+Employee Email Id:  makhija@gmail.com
+Employee Phone No.:  7807848053
+Employee Address:  manali 
+Employee Post:  developer 
+Employee Salary:  20200
+
+
+Employee Id:  25
+Employee Name:  Shivansh Banyal
+Employee Email Id:  shivanshbania@gmail.com
+Employee Phone No.:  9816034567
+Employee Address:  SunderNagar Himachal Pradesh
+Employee Post:  Software Developer 
+Employee Salary:  3000000
+
+
+Employee Id:  34
+Employee Name:  makhi jhant
+Employee Email Id:  makhija123@gmail.com
+Employee Phone No.:  7006619194
+Employee Address:  jammu
+Employee Post:  ceo
+Employee Salary:  2500000
+
+
+Employee Id:  3456
+Employee Name:  renu 
+Employee Email Id:  renu@gmail.com
+Employee Phone No.:  9418484053
+Employee Address:  manali 
+Employee Post:  developer 
+Employee Salary:  20000
+
+
+Press Any key To Continue..g
+                        ************************************
+                        -->> Employee Management System <<--
+                        ************************************
+1. Add Employee
+2. Display Employee Record
+3. Update Employee Record
+4. Promote Employee Record
+5. Remove Employee Record
+6. Search Employee Record
+7. Exit
+
+                   -->> Choice Options: [1/2/3/4/5/6/7] <<--
+Enter your Choice: 3
+                           -->> Update Employee Record <<--
+
+Enter Employee Id: 25
+Enter Employee Email ID: shivanshbania@gmail.com
+Valid Email
+Enter Employee Phone No.: 9816978678
+Valid Phone Number
+Enter Employee Address: Manali
+Updated Employee Record
+Press Any Key To Continue..f
+                        ************************************
+                        -->> Employee Management System <<--
+                        ************************************
+1. Add Employee
+2. Display Employee Record
+3. Update Employee Record
+4. Promote Employee Record
+5. Remove Employee Record
+6. Search Employee Record
+7. Exit
+
+                   -->> Choice Options: [1/2/3/4/5/6/7] <<--
+Enter your Choice: 5
+                           -->> Remove Employee Record <<--
+
+Enter Employee Id: 34
+Employee Removed
+Press Any key To Continue..f
+                        ************************************
+                        -->> Employee Management System <<--
+                        ************************************
+1. Add Employee
+2. Display Employee Record
+3. Update Employee Record
+4. Promote Employee Record
+5. Remove Employee Record
+6. Search Employee Record
+7. Exit
+
+                   -->> Choice Options: [1/2/3/4/5/6/7] <<--
+Enter your Choice: 6
+                           -->> Search Employee Record <<--
+
+Enter Employee Id: 25
+Employee Id:  25
+Employee Name:  Shivansh Banyal
+Employee Email Id:  shivanshbania@gmail.com
+Employee Phone No.:  9816978678
+Employee Address:  Manali
+Employee Post:  Software Developer 
+Employee Salary:  3000000
+
+
+Press Any key To Continue..f
+                        ************************************
+                        -->> Employee Management System <<--
+                        ************************************
+1. Add Employee
+2. Display Employee Record
+3. Update Employee Record
+4. Promote Employee Record
+5. Remove Employee Record
+6. Search Employee Record
+7. Exit
+
+                   -->> Choice Options: [1/2/3/4/5/6/7] <<--
+Enter your Choice: 7
+                                          Have A NIce Day :)
